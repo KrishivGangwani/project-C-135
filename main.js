bf70274b8e68f28bf2b1e77fe0691d518a6b12c8
KrishivGangwani <77724297+KrishivGangwani@users.noreply.github.com>
2022-05-27 12:40:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
   status1 = "";
+  objects = [];
 
 
 function setup() {
@@ -22,7 +23,44 @@ function modelLoaded() {
   status1 = true;
 }
 
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
 function draw() {
   image(video, 0, 0, 380, 380);
-      
-}
+  if(status1 != "")
+      {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status: Object Detected";
+          
+          fill("red");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("red");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+         
+          if(objects[i].label == object_name)
+          {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("os").innerHTML = object_name + " Found";
+            synth = window.speechSynthesis;
+            utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+            synth.speak(utterThis);
+          }
+          else
+          {
+            document.getElementById("os").innerHTML = object_name + " Not Found";
+          }          
+         }
+    
+        }
+              }
